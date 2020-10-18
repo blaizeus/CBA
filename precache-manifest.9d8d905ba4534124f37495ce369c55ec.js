d67da70479a1ca40da8d26bc53c4c05f77c89b0c
Blaise Vellozzo <bvellozzo@gmail.com>
2020-10-18 15:13:34 +0200

    Updates

---
+++ precache-manifest.9d8d905ba4534124f37495ce369c55ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cdc9e79940b4417b9c15913411a5bc",
+    "url": "/CBA/index.html"
+  },
+  {
+    "revision": "b30399f67bd334087575",
+    "url": "/CBA/static/js/2.87301cac.chunk.js"
+  },
+  {
+    "revision": "ce53a315179bf1b31de3d98da26d8898",
+    "url": "/CBA/static/js/2.87301cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e34ac572f4427bd8aef",
+    "url": "/CBA/static/js/main.b6ceb0c0.chunk.js"
+  },
+  {
+    "revision": "9588397bee06938a5015",
+    "url": "/CBA/static/js/runtime-main.fe4b0007.js"
+  }
+]);
