531568174bcba7a4765465b2fd49232802c18bd2
Blaise Vellozzo <bvellozzo@gmail.com>
2020-11-06 10:16:19 +0100

    update incorrect result message

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -7,7 +7,7 @@ function Result(props)
     return <div className="result">
 
         <p>The correct choice is:</p>
-        <h1>{props.correctChoice}Pizza</h1>
+        <h1>{props.correctChoice}</h1>
         <p>Enjoy your day, knowing that you've made the right decision!</p>
         <hr className="my-4"/>    
         <a className="btn-custom" href="/CBA">Return Home</a>
